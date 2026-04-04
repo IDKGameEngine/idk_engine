301c8e45873d7384e3ee398653dfa2e39efd057c
mellic03 <mellic03@gmail.com>
2026-04-04 17:07:35 +1000

    rip

---
+++ src/AppRuntime.cpp
@@ -0,0 +1,49 @@
+#include "idk/AppRuntime.hpp"
+#include "idk/core/log.hpp"
+#include <filesystem>
+#include <SDL3/SDL.h>
+
+
+bool idk::AppRuntime::Init()
+{
+    VLOG_INFO("[AppRuntime::Init] A");
+
+    if (false == SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_CAMERA))
+        VLOG_FATAL("{}", SDL_GetError());
+
+    std::filesystem::current_path(std::filesystem::path(SDL_GetBasePath()));
+
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE))
+        VLOG_ERROR("{}", SDL_GetError());
+
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4))
+        VLOG_ERROR("{}", SDL_GetError());
+
+    if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6))
+        VLOG_ERROR("{}", SDL_GetError());
+
+    if (!SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1))
+        VLOG_ERROR("{}", SDL_GetError());
+
+    if (!SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24))
+        VLOG_ERROR("{}", SDL_GetError());
+
+    if (!SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8))
+        VLOG_ERROR("{}", SDL_GetError());
+
+    VLOG_INFO("[AppRuntime::Init] B");
+
+    return true;
+}
+
+
+uint64_t idk::AppRuntime::GetSysTimeMs()
+{
+    return SDL_GetTicks();
+}
+
+uint64_t idk::AppRuntime::GetSysTimeNs()
+{
+    return SDL_GetTicksNS();
+}
+
--- src/adapter.cpp
+++
@@ -1,6 +0,0 @@
-#include "idk/core/adapter.hpp"
-#include <SDL3/SDL_timer.h>
-
-uint64_t idk_adapter::GetSysTimeMs() { return SDL_GetTicks(); }
-uint64_t idk_adapter::GetSysTimeNs() { return SDL_GetTicksNS(); }
-
